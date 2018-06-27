7926380846775facd90f18a1acd7e4449ffb7a37
Mironov Sergey <grrwlf@gmail.com>
2018-06-27 18:08:21 +0300

    tvm.nix: moved unused expression to comments

--- default.nix
+++ default.nix
@@ -1,3 +1,60 @@
 (import ./tvm-llvm.nix {}).shell
 
+/*
+  Old version of expressions
+
+  tvm = stdenv.mkDerivation rec {
+    name = "tvm";
+    src = ../tvm-clean;
+    buildInputs = with pkgs; [cmake] ++ tvmDeps;
+    cmakeFlags = tvmCmakeFlags;
+  };
+
+  tvm-python = pp.buildPythonPackage rec {
+    pname = "tvm";
+    version = "0.8";
+    name = "${pname}-${version}";
+    src = ../tvm-clean/python;
+    buildInputs = with pkgs; with pp; [tvm decorator numpy tornado];
+
+    preConfigure = ''
+      export LD_LIBRARY_PATH="${tvm}/lib:$LD_LIBRARY_PATH";
+    '';
+  };
+
+  tvm-python-topi = pp.buildPythonPackage rec {
+    pname = "tvm";
+    version = "0.8";
+    name = "${pname}-${version}";
+    src = ../tvm-clean;
+    buildInputs = with pkgs; with pp; [
+      tvm tvm-python decorator numpy tornado
+      scipy nose
+    ];
+
+    preConfigure = ''
+      cd topi/python
+      export LD_LIBRARY_PATH="${tvm}/lib:$LD_LIBRARY_PATH";
+    '';
+
+    doCheck=false;
+  };
+
+  nnvm = stdenv.mkDerivation {
+    name = "nnvm";
+
+    src = ../tvm-clean;
+
+    cmakeFlags = "-DBUILD_STATIC_NNVM=On";
+
+    buildInputs = with pkgs; with pp; [
+      cmake
+      python
+      setuptools
+      gfortran
+    ];
+  };
+
+
+*/
 
--- tvm.nix
+++ tvm.nix
@@ -18,58 +18,6 @@ in
 
 rec {
 
-  tvm = stdenv.mkDerivation rec {
-    name = "tvm";
-    src = ../tvm-clean;
-    buildInputs = with pkgs; [cmake] ++ tvmDeps;
-    cmakeFlags = tvmCmakeFlags;
-  };
-
-  tvm-python = pp.buildPythonPackage rec {
-    pname = "tvm";
-    version = "0.8";
-    name = "${pname}-${version}";
-    src = ../tvm-clean/python;
-    buildInputs = with pkgs; with pp; [tvm decorator numpy tornado];
-
-    preConfigure = ''
-      export LD_LIBRARY_PATH="${tvm}/lib:$LD_LIBRARY_PATH";
-    '';
-  };
-
-  tvm-python-topi = pp.buildPythonPackage rec {
-    pname = "tvm";
-    version = "0.8";
-    name = "${pname}-${version}";
-    src = ../tvm-clean;
-    buildInputs = with pkgs; with pp; [
-      tvm tvm-python decorator numpy tornado
-      scipy nose
-    ];
-
-    preConfigure = ''
-      cd topi/python
-      export LD_LIBRARY_PATH="${tvm}/lib:$LD_LIBRARY_PATH";
-    '';
-
-    doCheck=false;
-  };
-
-  nnvm = stdenv.mkDerivation {
-    name = "nnvm";
-
-    src = ../tvm-clean;
-
-    cmakeFlags = "-DBUILD_STATIC_NNVM=On";
-
-    buildInputs = with pkgs; with pp; [
-      cmake
-      python
-      setuptools
-      gfortran
-    ];
-  };
-
 
   mktags = pkgs.writeShellScriptBin "mktags" ''
     find -name '*cc' -or -name '*hpp' -or -name '*h' | \
