c05d1a4626e4e9cf4760c0549fcc0c47121a7189
Sergey Mironov <grrwlf@gmail.com>
2018-07-04 17:57:13 +0300

    mironov: update minst TF example

--- src/mironov/tf/mnist.py
+++ src/mironov/tf/mnist.py
@@ -35,70 +35,74 @@ num_input = 784 # MNIST data input (img shape: 28*28)
 num_classes = 10 # MNIST total classes (0-9 digits)
 
 
-# Define the neural network
-def neural_net(x_dict):
-    # TF Estimator input is a dict, in case of multiple inputs
-    x = x_dict['images']
-    # Hidden fully connected layer with 256 neurons
-    layer_1 = tf.layers.dense(x, n_hidden_1)
-    # Hidden fully connected layer with 256 neurons
-    layer_2 = tf.layers.dense(layer_1, n_hidden_2)
-    # Output fully connected layer with a neuron for each class
-    out_layer = tf.layers.dense(layer_2, num_classes)
-    return out_layer
-
 
 # Define the model function (following TF Estimator Template)
 def model_fn(features, labels, mode):
-    # Build the neural network
-    logits = neural_net(features)
+  # TF Estimator input is a dict, in case of multiple inputs
+  x = features['images']
+  # Hidden fully connected layer with 256 neurons
+  layer_1 = tf.layers.dense(x, n_hidden_1)
+  # Hidden fully connected layer with 256 neurons
+  layer_2 = tf.layers.dense(layer_1, n_hidden_2)
+  # Output fully connected layer with a neuron for each class
+  logits = tf.layers.dense(layer_2, num_classes)
 
-    # Predictions
-    pred_classes = tf.argmax(logits, axis=1)
-    pred_probas = tf.nn.softmax(logits)
+  # Predictions
+  pred_classes = tf.argmax(logits, axis=1)
+  pred_probas = tf.nn.softmax(logits)
 
-    # If prediction mode, early return
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode, predictions=pred_classes)
+  # If prediction mode, early return
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(mode, predictions=pred_classes)
 
-        # Define loss and optimizer
-    loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=logits, labels=tf.cast(labels, dtype=tf.int32)))
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-    train_op = optimizer.minimize(loss_op,
-                                  global_step=tf.train.get_global_step())
+      # Define loss and optimizer
+  loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+    logits=logits, labels=tf.cast(labels, dtype=tf.int32)))
+  optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+  train_op = optimizer.minimize(loss_op,
+                                global_step=tf.train.get_global_step())
 
-    # Evaluate the accuracy of the model
-    acc_op = tf.metrics.accuracy(labels=labels, predictions=pred_classes)
+  # Evaluate the accuracy of the model
+  acc_op = tf.metrics.accuracy(labels=labels, predictions=pred_classes)
 
-    # TF Estimators requires to return a EstimatorSpec, that specify
-    # the different ops for training, evaluating, ...
-    estim_specs = tf.estimator.EstimatorSpec(
-        mode=mode,
-        predictions=pred_classes,
-        loss=loss_op,
-        train_op=train_op,
-        eval_metric_ops={'accuracy': acc_op})
+  # TF Estimators requires to return a EstimatorSpec, that specify
+  # the different ops for training, evaluating, ...
+  estim_specs = tf.estimator.EstimatorSpec(
+    mode=mode,
+    predictions=pred_classes,
+    loss=loss_op,
+    train_op=train_op,
+    eval_metric_ops={'accuracy': acc_op})
 
-    return estim_specs
+  return estim_specs
 
-# Build the Estimator
-model = tf.estimator.Estimator(model_fn)
+class Model:
+  def __init__(self):
+    pass
 
-# Define the input function for training
-input_fn = tf.estimator.inputs.numpy_input_fn(
+def load(m=None):
+  if m is None:
+    m=Model()
+  # Build the Estimator
+  m.model = tf.estimator.Estimator(model_fn)
+  return m
+
+def train(m):
+  # Define the input function for training
+  input_fn = tf.estimator.inputs.numpy_input_fn(
     x={'images': mnist.train.images}, y=mnist.train.labels,
     batch_size=batch_size, num_epochs=None, shuffle=True)
-# Train the Model
-model.train(input_fn, steps=num_steps)
+  # Train the Model
+  m.model.train(input_fn, steps=num_steps)
 
-# Evaluate the Model
-# Define the input function for evaluating
-input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={'images': mnist.test.images}, y=mnist.test.labels,
-    batch_size=batch_size, shuffle=False)
-# Use the Estimator 'evaluate' method
-e = model.evaluate(input_fn)
+def eval(m):
+  # Evaluate the Model
+  # Define the input function for evaluating
+  input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={'images': mnist.test.images}, y=mnist.test.labels,
+      batch_size=batch_size, shuffle=False)
 
-print("Testing Accuracy:", e['accuracy'])
+  # Use the Estimator 'evaluate' method
+  e = m.model.evaluate(input_fn)
+  print("Testing Accuracy:", e['accuracy'])
 
