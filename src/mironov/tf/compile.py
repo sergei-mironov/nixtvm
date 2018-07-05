c83f438ec5c1313f6121e260f66482b1396157ce
Sergey Mironov <grrwlf@gmail.com>
2018-07-05 19:49:44 +0300

    minst: minor adjustments

--- src/mironov/tf/compile.py
+++ src/mironov/tf/compile.py
@@ -1,11 +1,21 @@
+"""
+
+References: https://www.tensorflow.org/programmers_guide/saved_model#using_savedmodel_with_estimators
+"""
 
 import nnvm
 import tvm
+import tensorflow as tf
 
 from tf.mnist import Model, load
 
 
 def compile(m:Model):
   """ Take trained model, return tvm scheme """
-  pass
+  with tf.Session() as sess:
+      graph_def = tf.graph_util.convert_variables_to_constants(
+          sess,
+          sess.graph.as_graph_def(add_shapes=True),
+          ['out'],
+          )
 
--- src/mironov/tf/mnist.py
+++ src/mironov/tf/mnist.py
@@ -53,9 +53,11 @@ def model_fn(features, labels, mode):
 
   # If prediction mode, early return
   if mode == tf.estimator.ModeKeys.PREDICT:
-    return tf.estimator.EstimatorSpec(mode, predictions=pred_classes)
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        predictions=pred_classes)
 
-      # Define loss and optimizer
+  # Define loss and optimizer
   loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
     logits=logits, labels=tf.cast(labels, dtype=tf.int32)))
   optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
@@ -67,14 +69,12 @@ def model_fn(features, labels, mode):
 
   # TF Estimators requires to return a EstimatorSpec, that specify
   # the different ops for training, evaluating, ...
-  estim_specs = tf.estimator.EstimatorSpec(
+  return tf.estimator.EstimatorSpec(
     mode=mode,
     predictions=pred_classes,
     loss=loss_op,
     train_op=train_op,
     eval_metric_ops={'accuracy': acc_op})
-
-  return estim_specs
 
 class Model:
   def __init__(self):
@@ -84,7 +84,7 @@ def load(m=None):
   if m is None:
     m=Model()
   # Build the Estimator
-  m.model = tf.estimator.Estimator(model_fn)
+  m.estimator = tf.estimator.Estimator(model_fn)
   return m
 
 def train(m):
@@ -93,7 +93,7 @@ def train(m):
     x={'images': mnist.train.images}, y=mnist.train.labels,
     batch_size=batch_size, num_epochs=None, shuffle=True)
   # Train the Model
-  m.model.train(input_fn, steps=num_steps)
+  m.estimator.train(input_fn, steps=num_steps)
 
 def eval(m):
   # Evaluate the Model
@@ -103,6 +103,6 @@ def eval(m):
       batch_size=batch_size, shuffle=False)
 
   # Use the Estimator 'evaluate' method
-  e = m.model.evaluate(input_fn)
+  e = m.estimator.evaluate(input_fn)
   print("Testing Accuracy:", e['accuracy'])
 
