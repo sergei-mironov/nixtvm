7b018bbfd461a06a7c4c8690ab8f1d39fff1c547
Sergey Mironov <mironov.sergey@huawei.com>
2018-08-21 14:12:59 +0300

    Dockerfile.android: add draft dockerfile

--- rundocker.sh
+++ rundocker.sh
@@ -1,109 +1,98 @@
 #!/usr/bin/env bash
 
 # Uncomment to debug
-# set -x
+set -x
+
+while test -n "$1" ; do
+  case "$1" in
+    -h|--help)
+      echo "Usage: $0 [--noport] [SUFFIX]" >&2
+      exit 1
+      ;;
+    --noport)
+      NOPORT=y
+      ;;
+    *)
+      SUFFIX="$1"
+      ;;
+  esac
+  shift
+done
 
 UID=`id --user`
+test -z "$SUFFIX" && SUFFIX="dev"
+test -z "$NOPORT" && NOPORT=n
+test -z "$DOCKER_WORKSPACE" && DOCKER_WORKSPACE=`pwd`
+test -z "$DOCKER_COMMAND" && DOCKER_COMMAND="/bin/bash"
 DOCKER_CONTEXT_PATH="./tvm/docker"
-DOCKER_EXTRA_PARAMS+=('-it')
-if test -n "$1"; then
-  CONTAINER_TYPE="$1"
+DOCKERFILE_PATH="./Dockerfile.${SUFFIX}"
+
+if echo "$SUFFIX" | grep -q gpu ; then
+  CONTAINER_TYPE=gpu
+  DOCKER_BINARY="nvidia-docker"
 else
-  CONTAINER_TYPE="dev"
-fi
-DOCKERFILE_PATH="./Dockerfile.${CONTAINER_TYPE}"
-COMMAND="/bin/bash"
-if test -z "$NOPORT" ; then
-  PORT_TENSORBOARD=`expr 6000 + $UID - 1000`
-  PORT_JUPYTER=`expr 8000 + $UID - 1000`
-  DOCKER_PORT_ARGS="-p 0.0.0.0:$PORT_TENSORBOARD:6006 -p 0.0.0.0:$PORT_JUPYTER:8888"
-fi
-RM="--rm" # remove image after use
-
-# Use nvidia-docker if the container is GPU.
-if [[ "${CONTAINER_TYPE}" == *"gpu"* ]]; then
-    DOCKER_BINARY="nvidia-docker"
-else
-    DOCKER_BINARY="docker"
+  CONTAINER_TYPE=cpu
+  DOCKER_BINARY="docker"
 fi
 
-# Set up WORKSPACE and BUILD_TAG. Jenkins will set them for you or we pick
-# reasonable defaults if you run it outside of Jenkins.
-WORKSPACE="${WORKSPACE:-`pwd`}"
-BUILD_TAG="${BUILD_TAG:-tvm-ci}"
+DOCKER_IMG_NAME=$(echo "hitvm.${CONTAINER_TYPE}" | sed -e 's/=/_/g' -e 's/,/-/g' | tr '[:upper:]' '[:lower:]')
 
-# Determine the docker image name
-DOCKER_IMG_NAME="${BUILD_TAG}.${CONTAINER_TYPE}"
-
-# Under Jenkins matrix build, the build tag may contain characters such as
-# commas (,) and equal signs (=), which are not valid inside docker image names.
-DOCKER_IMG_NAME=$(echo "${DOCKER_IMG_NAME}" | sed -e 's/=/_/g' -e 's/,/-/g')
-
-# Convert to all lower-case, as per requirement of Docker image names
-DOCKER_IMG_NAME=$(echo "${DOCKER_IMG_NAME}" | tr '[:upper:]' '[:lower:]')
-
-# HACK: setup a proxy
-if test -n "$https_proxy" ; then
-  all_proxy="http://10.122.85.159:3128"
-  PROXY="--build-arg=http_proxy=$all_proxy --build-arg=https_proxy=$all_proxy --build-arg=ftp_proxy=$all_proxy"
-else
-  PROXY=""
+if test -z "$DOCKER_PROXY_ARGS" ; then
+  if test -n "$https_proxy" ; then
+    PROXY_HOST=`echo $https_proxy | sed 's@.*//\(.*\):.*@\1@'`
+    PROXY_PORT=`echo $https_proxy | sed 's@.*//.*:\(.*\)@\1@'`
+    DOCKER_PROXY_ARGS="--build-arg=http_proxy=$https_proxy --build-arg=https_proxy=$https_proxy --build-arg=ftp_proxy=$https_proxy"
+  fi
 fi
 
-# HACK: Remap detach to Ctrl+e,e
+# Copy additional files to context
+cp -f Huawei.crt "$DOCKER_CONTEXT_PATH"
+for f in _dist/* ; do
+  echo "$DOCKER_CONTEXT_PATH/`basename $f` -> $f"
+  ln $f $DOCKER_CONTEXT_PATH 2>/dev/null
+done
+
+docker build ${DOCKER_PROXY_ARGS} -t ${DOCKER_IMG_NAME} \
+  -f "${DOCKERFILE_PATH}" "${DOCKER_CONTEXT_PATH}"
+
+if [[ $? != "0" ]]; then
+  echo "ERROR: docker build failed."
+  exit 1
+fi
+
+# Remap detach to Ctrl+e,e
 mkdir /tmp/docker-$UID || true
 cat >/tmp/docker-$UID/config.json <<EOF
 { "detachKeys": "ctrl-e,e" }
 EOF
-CFG="--config /tmp/docker-$UID"
+DOCKER_CFG="--config /tmp/docker-$UID"
 
-for f in _dist/* ; do
-  echo "$DOCKER_CONTEXT_PATH/`basename $f` -> $f"
-  ln $f $DOCKER_CONTEXT_PATH
-done
-
-# Build the docker container.
-echo "Building container (${DOCKER_IMG_NAME})..."
-docker build $PROXY -t ${DOCKER_IMG_NAME} \
-    -f "${DOCKERFILE_PATH}" "${DOCKER_CONTEXT_PATH}"
-
-# Check docker build status
-if [[ $? != "0" ]]; then
-    echo "ERROR: docker build failed."
-    exit 1
+if test "$NOPORT" = "y"; then
+  PORT_TENSORBOARD=`expr 6000 + $UID - 1000`
+  PORT_JUPYTER=`expr 8000 + $UID - 1000`
+  DOCKER_PORT_ARGS="-p 0.0.0.0:$PORT_TENSORBOARD:6006 -p 0.0.0.0:$PORT_JUPYTER:8888"
+  echo
+  echo "*****************************"
+  echo "Your Jupyter port: ${PORT_JUPYTER}"
+  echo "Your Tensorboard port: ${PORT_TENSORBOARD}"
+  echo "*****************************"
 fi
 
-echo "PROXY: ${PROXY}"
-echo "WORKSPACE: ${WORKSPACE}"
-echo "DOCKER_EXTRA_PARAMS: ${DOCKER_EXTRA_PARAMS[@]}"
-echo "COMMAND: ${COMMAND[@]}"
-echo "CONTAINER_TYPE: ${CONTAINER_TYPE}"
-echo "BUILD_TAG: ${BUILD_TAG}"
-echo "DOCKER_IMG_NAME: ${DOCKER_IMG_NAME}"
-if test -z "$NOPORT"; then
-echo
-echo "*****************************"
-echo "Your Jupyter port: ${PORT_JUPYTER}"
-echo "Your Tensorboard port: ${PORT_TENSORBOARD}"
-echo "*****************************"
-fi
-
-set -x
-
-${DOCKER_BINARY} $CFG run $RM --pid=host \
-    -v ${WORKSPACE}:/workspace \
-    -w /workspace \
-    -e "CI_BUILD_HOME=/workspace" \
-    -e "CI_BUILD_USER=$(id -u -n)" \
-    -e "CI_BUILD_UID=$(id -u)" \
-    -e "CI_BUILD_GROUP=$(id -g -n)" \
-    -e "CI_BUILD_GID=$(id -g)" \
-    -e "DISPLAY=$DISPLAY" \
-    -e "http_proxy=$http_proxy" \
-    -e "https_proxy=$https_proxy" \
-    ${DOCKER_PORT_ARGS} \
-    ${DOCKER_EXTRA_PARAMS[@]} \
-    ${DOCKER_IMG_NAME} \
-    bash tvm/docker/with_the_same_user \
-    ${COMMAND[@]}
+${DOCKER_BINARY} $DOCKER_CFG run --rm --pid=host \
+  -v ${DOCKER_WORKSPACE}:/workspace \
+  -w /workspace \
+  -e "CI_BUILD_HOME=/workspace" \
+  -e "CI_BUILD_USER=$(id -u -n)" \
+  -e "CI_BUILD_UID=$(id -u)" \
+  -e "CI_BUILD_GROUP=$(id -g -n)" \
+  -e "CI_BUILD_GID=$(id -g)" \
+  -e "MAVEN_OPTS=-Dhttps.proxyHost=$PROXY_HOST -Dhttps.proxyPort=$PROXY_PORT -Dmaven.wagon.http.ssl.insecure=true" \
+  -e "DISPLAY=$DISPLAY" \
+  -e "http_proxy=$http_proxy" \
+  -e "https_proxy=$https_proxy" \
+  ${DOCKER_PORT_ARGS} \
+  -it \
+  ${DOCKER_IMG_NAME} \
+  bash tvm/docker/with_the_same_user \
+  ${DOCKER_COMMAND}
 
