8379d7ad7142f995b7b019bfe8edfce58f3845e6
Sergey Mironov <grrwlf@gmail.com>
2018-10-09 01:17:36 +0300

    train, autodiff simple example

--- src/mironov/train/train_autodiff.py
+++ src/mironov/train/train_autodiff.py
@@ -6,22 +6,16 @@ import time
 
 import keras.datasets.mnist as mnist
 
-def dataset_mnist():
-  """
-  To classify images using a recurrent neural network, we consider every image
-  row as a sequence of pixels. Because MNIST image shape is 28*28px, we will then
-  handle 28 sequences of 28 steps for every sample.
-  """
+def minst_load():
   (X,y),(Xte,yte) = mnist.load_data("/tmp/mnist.npz")
   return (X,y),(Xte,yte)
-
 
 
 def get_shape(tensor):
   return [tvm.ir_pass.Simplify(s).value for s in tensor.shape]
 
 
-def run(out, inp, args=[], in_range=(-10,10)):
+def conv_run(out, inp, args=[], in_range=(-10,10)):
   sout = tvm.create_schedule(out.op)
   mout = tvm.build(sout, [out] + inp + args)
 
@@ -37,7 +31,7 @@ def run(out, inp, args=[], in_range=(-10,10)):
   print("BUILD TIME: ", time.time() - t)
   return mjac
 
-def build():
+def conv_build():
   batch_size = 1
   num_classes = 10
 
@@ -69,8 +63,44 @@ def build():
 
   weights = [w1, b1, w2, b2, w3, b3, w4, b4]
 
-  return run(t, weights, [x, y], in_range=(-1.0, 1.0))
+  return conv_run(t, weights, [x, y], in_range=(-1.0, 1.0))
 
+
+def ex1_build():
+  """
+  gdb --args `which python`  -c 'from train.train_autodiff import * ; ex1_build();'
+
+  (gdb) bt
+  #0  0x00007ffff7e16048 in default_function ()
+  #1  0x00007fffdfbdd0c0 in tvm::runtime::WrapPackedFunc(int (*)(void*, int*, int), std::shared_ptr<tvm::runtime::ModuleNode> const&)::{lambda(tvm::runtime::TVMArgs, tvm::runtime::TVMRetValue*)#1}::operator()(tvm::runtime::TVMArgs, tvm::runtime::TVMRetValue*) const [clone .isra.71] ()
+     from /home/grwlf/proj/nixtvm/tvm/build-native/libtvm.so
+  #2  0x00007fffdfbc48de in TVMFuncCall () from /home/grwlf/proj/nixtvm/tvm/build-native/libtvm.so
+  #3  0x00007fffef013a3e in ffi_call_unix64 () from /nix/store/6z0spj65xwl2wf7dbn0fd8jxcka3yx3h-libffi-3.2.1/lib/libffi.so.6
+  #4  0x00007fffef012a03 in ffi_call () from /nix/store/6z0spj65xwl2wf7dbn0fd8jxcka3yx3h-libffi-3.2.1/lib/libffi.so.6
+  #5  0x00007fffef22850d in _ctypes_callproc ()
+  """
+
+  x = tvm.placeholder((1,))
+  k = tvm.placeholder((1,))
+  y = tvm.compute((1,), lambda i: x[i]*k[0], name='y')
+
+  ones = topi.full_like(y, 1.0)
+  jac = list(tvm.ir_pass.JacobianRecursive(y, [x], ones))
+  print('jac',type(jac),jac[0])
+
+  sjac = tvm.create_schedule([j.op for j in jac])
+  mjac = tvm.build(sjac, jac + [y,x,k])  # TODO: Why should we specify y,x,k?
+  # print(type(mjac), mjac.get_source())
+
+
+  args = [tvm.nd.empty(get_shape(i), j.dtype) for i, j in zip([y], jac)] +\
+         [tvm.nd.array(np.array([1.0]).astype(a.dtype)) for a in [x,k]]
+  print('args',type(args),len(args),args)
+
+  mjac(*args)
+
+  print('Done??')
+  print(jout.asnumpy())
 
 
 
