ec900b4ea968acc277622d289c2737bdf01e338a
Sergey Mironov <mironov.sergey@huawei.com>
2018-10-11 16:47:59 +0300

    train, add conv stub

--- src/mironov/train/autodiff_conv.py
+++ src/mironov/train/autodiff_conv.py
@@ -1,14 +1,21 @@
 import tvm
 import topi
 import numpy as np
-from nnvm.testing.check_computation import check_numerical_grads
 import time
-
 import keras.datasets.mnist as mnist
 
-def minst_load():
-  (X,y),(Xte,yte) = mnist.load_data("/tmp/mnist.npz")
-  return (X,y),(Xte,yte)
+from train.runners import run_tvm
+
+
+def mnist_load():
+  (Xtr,ytr),(Xte,yte) = mnist.load_data("/tmp/mnist.npz")
+  return (Xtr,ytr),(Xte,yte)
+
+
+def mnist_img(i):
+  return mnist_load()[0][0][i]
+
+
 
 def get_shape(tensor):
   return [tvm.ir_pass.Simplify(s).value for s in tensor.shape]
@@ -31,35 +38,51 @@ def conv_run(out, inp, args=[], in_range=(-10,10)):
   return mjac
 
 def conv_build():
-  batch_size = 1
   num_classes = 10
+  batch_size = 1
+  img_h = 28
+  img_w = 28
+  img_c = 1
 
-  features = 4
-  dense_units = 16
+  f1_c = 1
 
-  x = tvm.placeholder((batch_size, 28, 28, 1))
-  y = tvm.placeholder((batch_size, num_classes))
-  w1 = tvm.placeholder((features, 1, 3, 5))
-  b1 = tvm.placeholder((features,))
-  w2 = tvm.placeholder((features, features, 3, 5))
-  b2 = tvm.placeholder((features,))
-  b3 = tvm.placeholder((dense_units,))
-  w4 = tvm.placeholder((num_classes, dense_units))
-  b4 = tvm.placeholder((num_classes,))
+  x = tvm.placeholder((batch_size, img_h, img_w, img_c),name='x')
+  w1 = tvm.placeholder((3,3,img_c,f1_c),name='w1')
+  # b1 = tvm.placeholder((f1_c,))
+  t = topi.nn.conv2d(x, w1, 1, 0, layout='NHWC', out_dtype=tvm.float32)
 
-  t = topi.transpose(x, [0, 3, 1, 2])
-  t = topi.nn.relu(topi.nn.conv2d(t, w1, 1, 0) + topi.reshape(b1, (1, features, 1, 1)))
-  t = topi.nn.relu(topi.nn.conv2d(t, w2, 1, 0) + topi.reshape(b2, (1, features, 1, 1)))
-  t = topi.nn.pool(t, [2, 2], [2, 2], [0, 0, 0, 0], 'avg')
-  t = topi.transpose(t, [0, 2, 3, 1])
-  t = topi.nn.flatten(t)
-  w3 = tvm.placeholder((dense_units, get_shape(t)[1]))
-  t = topi.nn.relu(topi.nn.dense(t, w3, b3))
-  t = topi.nn.dense(t, w4, b4)
+  npt = run_tvm(
+      0,1,
+      { x:mnist_img(3).reshape(get_shape(x)).astype(np.float32)
+      , w1:np.zeros(get_shape(w1)).astype(np.float32)
+      },t)
 
-  t = - topi.sum(y * topi.nn.log_softmax(t)) / batch_size
+  print(npt.last_data)
+  return npt
 
-  weights = [w1, b1, w2, b2, w3, b3, w4, b4]
 
-  return conv_run(t, weights, [x, y], in_range=(-1.0, 1.0))
+  # w2 = tvm.placeholder((features, features, 3, 5))
+  # b2 = tvm.placeholder((features,))
+  # b3 = tvm.placeholder((dense_units,))
+  # w4 = tvm.placeholder((num_classes, dense_units))
+  # b4 = tvm.placeholder((num_classes,))
+  # y = tvm.placeholder((batch_size, num_classes))
+
+  # t = topi.nn.relu(topi.nn.conv2d(t, w1, 1, 0, layout='NHWC') + topi.reshape(b1, (1, features, 1, 1)))
+
+  # t = topi.transpose(x, [0, 3, 1, 2])
+  # t = topi.nn.relu(topi.nn.conv2d(t, w1, 1, 0) + topi.reshape(b1, (1, features, 1, 1)))
+  # t = topi.nn.relu(topi.nn.conv2d(t, w2, 1, 0) + topi.reshape(b2, (1, features, 1, 1)))
+  # t = topi.nn.pool(t, [2, 2], [2, 2], [0, 0, 0, 0], 'avg')
+  # t = topi.transpose(t, [0, 2, 3, 1])
+  # t = topi.nn.flatten(t)
+  # w3 = tvm.placeholder((dense_units, get_shape(t)[1]))
+  # t = topi.nn.relu(topi.nn.dense(t, w3, b3))
+  # t = topi.nn.dense(t, w4, b4)
+
+  # t = - topi.sum(y * topi.nn.log_softmax(t)) / batch_size
+
+  # weights = [w1, b1, w2, b2, w3, b3, w4, b4]
+
+  # return conv_run(t, weights, [x, y], in_range=(-1.0, 1.0))
 
