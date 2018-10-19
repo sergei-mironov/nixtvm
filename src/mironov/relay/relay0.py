45e7fdf8e0cfd380034f34cf626a1eeb3b815921
Sergey Mironov <mironov.sergey@huawei.com>
2018-10-19 16:06:22 +0300

    relay: fix Makefile, add debug_print

--- src/mironov/relay/relay0.py
+++ src/mironov/relay/relay0.py
@@ -6,6 +6,7 @@ lib.test_dispatch()
 
 from tvm import relay
 from tvm._ffi.function import list_global_func_names, get_global_func
+from tvm.relay.expr import debug_print
 
 def test1():
   x=relay.Var('x')
@@ -24,3 +25,20 @@ def test2():
   x=relay.Var('x')
   y=relay.exp(x)
   f(y)
+
+
+def test_where():
+  """ from test_op_level4.py, added debug printing """
+  ib = relay.ir_builder.IRBuilder()
+  cond = ib.param("cond", relay.TensorType((3, 4), "float32"))
+  x = ib.param("x", relay.TensorType((3, 4), "float32"))
+  y = ib.param("y", relay.TensorType((3, 4), "float32"))
+  with ib.function(cond, x, y) as func:
+      ib.ret(relay.where(cond, x, y))
+  ib.ret(func)
+  func = relay.ir_pass.infer_type(ib.env, func.to_func())
+
+  print(debug_print(ib.env, func))
+  ftype = func.checked_type
+  print(ftype)
+  assert ftype.ret_type == relay.TensorType((3, 4), "float32")
