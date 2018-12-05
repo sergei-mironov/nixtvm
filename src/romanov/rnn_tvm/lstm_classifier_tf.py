6e938308e3982a640e5ad7920a455fc5a09fbd31
Alexey Romanov <romanov.alexey1@huawei.com>
2018-12-05 13:08:02 +0300

    Minor refactoring for LSTM classifier

--- src/romanov/rnn_tvm/lstm_classifier_tf.py
+++ src/romanov/rnn_tvm/lstm_classifier_tf.py
@@ -5,9 +5,7 @@ Poor man's LSTM cell applied to MNIST. Use `train` function to train the model.
 import tensorflow as tf
 import numpy as np
 
-from typing import Any, List, Dict
 from keras.datasets import mnist
-from tensorflow import Tensor as TF_Tensor
 from tensorflow.python.ops import variables
 from tensorflow.python.saved_model.simple_save import simple_save
 
@@ -23,35 +21,8 @@ def lstm_gate(op, U, b, x):
     return op(tf.matmul(x, U) + b)
 
 
-def lstm_cell(Ug, bg, Ui, bi, Uf, bf, Uo, bo):
-    """ LSTM cell. Ideomatic TF code would define all the variable here """
-
-    def call(xt, st, ht):
-        input = tf.concat([xt, ht], 1)
-        g = lstm_gate(tf.tanh, Ug, bg, input)
-        i = lstm_gate(tf.sigmoid, Ui, bi, input)
-        f = lstm_gate(tf.sigmoid, Uf, bf, input)
-        o = lstm_gate(tf.sigmoid, Uo, bo, input)
-
-        st2 = st * f + g * i
-        ht2 = tf.tanh(st2) * o
-        return (st2, ht2)
-
-    return call
-
-
-def lstm_layer(cell, xs: List[TF_Tensor], s0, h0) -> List[TF_Tensor]:
-    h = h0
-    s = s0
-    hs = []
-    for i in range(len(xs)):
-        s, h = cell(xs[i], s, h)
-        hs.append(h)
-    return hs
-
-
-def model(num_timesteps: int, num_inputs: int, num_units: int, init=tf.initializers.glorot_uniform(),
-          bias_init=tf.zeros):
+def lstm_layer(num_timesteps: int, num_inputs: int, num_units: int,
+               init=tf.initializers.glorot_uniform(), bias_init=tf.zeros):
     """
     Create a single cell and replicate it `num_timesteps` times for training.
     Return X,[(batch_size,num_classes) x num_timesteps]
@@ -72,34 +43,46 @@ def model(num_timesteps: int, num_inputs: int, num_units: int, init=tf.initializ
     Uo = tf.Variable(init(U_shape))
     bo = tf.Variable(bias_init(b_shape))
 
-    cell = lstm_cell(Ug, bg, Ui, bi, Uf, bf, Uo, bo)
+    def cell(x_t, s_t, h_t):
+        xh_t = tf.concat([x_t, h_t], 1)
+        g = lstm_gate(tf.tanh, Ug, bg, xh_t)
+        i = lstm_gate(tf.sigmoid, Ui, bi, xh_t)
+        f = lstm_gate(tf.sigmoid, Uf, bf, xh_t)
+        o = lstm_gate(tf.sigmoid, Uo, bo, xh_t)
+
+        s_t1 = s_t * f + g * i
+        h_t1 = tf.tanh(s_t1) * o
+        return (s_t1, h_t1)
 
     xs = tf.unstack(X, num_timesteps, 1)
-    x = xs[0]
-    s_shape = tf.stack([x.shape[0].value or tf.shape(x)[0], num_units], name="s_shape")
-    s0 = tf.zeros(s_shape, dtype=np.float32)
-    h0 = s0
+    batch_size = tf.shape(X)[0]
+    s_shape = tf.stack([batch_size, num_units], name="s_shape")
 
-    outputs = lstm_layer(cell, xs, s0, h0)
+    s = tf.zeros(s_shape, dtype=np.float32)
+    h = s
+    outputs = []
+    for x in xs:
+        s, h = cell(x, s, h)
+        outputs.append(h)
+
     return X, outputs
 
 
-def model2(num_timesteps: int, num_inputs: int, num_classes: int, num_hidden: int,
-           init=tf.random_normal):
+def lstm_and_dense_layer(num_timesteps: int, num_inputs: int, num_classes: int, num_hidden: int,
+                         dense_init=tf.random_normal):
     """
-    Use `model` with 128 "classes", but translate them back to 10 classes via
-    dense layer.
+    Use `model` with `num_hidden` LSTM units, translate them `num_classes` classes using a dense layer.
     """
-    W = tf.Variable(init([num_hidden, num_classes]))
-    b = tf.Variable(init([1, num_classes]))
+    W = tf.Variable(dense_init([num_hidden, num_classes]))
+    b = tf.Variable(dense_init([1, num_classes]))
 
-    X, outputs = model(num_timesteps, num_inputs, num_units=num_hidden)
+    X, outputs = lstm_layer(num_timesteps, num_inputs, num_units=num_hidden)
     cls = tf.matmul(outputs[-1], W) + b
     return X, cls
 
 
 def mnist_load():
-    """ Load MNIST and convert its ys to one-hot encoding """
+    """ Load MNIST data and convert its ys to one-hot encoding """
     (Xl, yl), (Xt, yt) = mnist.load_data()
 
     def oh(y):
@@ -110,32 +93,33 @@ def mnist_load():
     def as_float(X):
         return X.astype(np.float32) / 255.0
 
-    Xl = as_float(Xl)
-    Xt = as_float(Xt)
-    return (Xl, oh(yl)), (Xt, oh(yt))
+    return (as_float(Xl), oh(yl)), (as_float(Xt), oh(yt))
 
 
-def train():
-    (Xl, yl), (Xt, yt) = mnist_load()
-    """ Main train """
+def train(Xl, yl, Xt, yt):
+    """ Main train
+    :param Xl training examples tensor, shape [num_examples, num_inputs, num_timesteps]
+    :param yl one-hot encoded training labels tensor, shape [num_examples, num_classes]
+    :param Xt test examples tensor, shape [num_examples, num_inputs, num_timesteps]
+    :param yt one-hot encoded test labels tensor, shape [num_examples, num_classes]
+    """
     batch_size = 64
-    num_timesteps = 28 # number of rows (each row in the image is considered as a timestep)
-    num_inputs = 28 # length of each row
+    num_timesteps = Xl.shape[2] # number of rows (each row in the image is considered as a timestep)
+    num_inputs = Xl.shape[1] # length of each row
     num_hidden = 128
-    num_classes = 10
+    num_classes = yl.shape[1]
     training_steps = 500 # TODO temporary to make faster, 10000 to actually train
     learning_rate = 0.001
     num_examples = Xl.shape[0]
     with tf.Session(graph=tf.Graph()) as sess:
-        X, logits = model2(num_timesteps, num_inputs, num_classes=num_classes, num_hidden=num_hidden,
-                           init=tf.random_normal)
+        X, lstm_out = lstm_and_dense_layer(num_timesteps, num_inputs, num_classes=num_classes, num_hidden=num_hidden)
         y = tf.placeholder(tf.float32, shape=(None, num_classes))
 
-        loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y))
+        loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=lstm_out, labels=y))
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
         train_op = optimizer.minimize(loss_op)
 
-        prediction = tf.nn.softmax(logits, name='prediction')
+        prediction = tf.nn.softmax(lstm_out, name='prediction')
         correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
         accuracy_op = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
@@ -181,4 +165,5 @@ def train():
 
 
 if __name__ == '__main__':
-    train()
+    (Xl, yl), (Xt, yt) = mnist_load()
+    train(Xl, yl, Xt, yt)
